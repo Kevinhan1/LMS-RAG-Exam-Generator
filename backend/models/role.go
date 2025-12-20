aa51ab2532fb09b5f7e9a62a9429ccb68dc3f43f
Ricky Junianto Wijaya <86522702+sLytherin131@users.noreply.github.com>
2025-12-21 04:08:57 +0700

    Delete backend directory

--- backend/models/role.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Role struct {
-	ID           int64  `json:"id"`
-	Name         string `json:"name"`
-	Description  string `json:"description"`
-	TimeCreated  int64  `json:"timecreated"`
-	TimeModified int64  `json:"timemodified"`
-}
